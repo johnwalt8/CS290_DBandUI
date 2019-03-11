7c7732739bc65a4f94d833f3f5361c9c0daa0c59
walter_johnson <walter_johnson@trimble.com>
2019-03-11 16:26:33 -0700

    mostly completed functionality without ajax

--- public/scripts/workouts.js
+++ public/scripts/workouts.js
@@ -12,7 +12,9 @@ const WOG = {
     submitButton: document.getElementById("submitButton"),
     workoutTBody: document.getElementById("workoutTBody"),
     createRow: null,
-    bindSubmit: null
+    fillRow: null, 
+    emptyForm: null,
+    bindSubmit: null, 
 };
 
 WOG.createIndex = (function () {
@@ -24,12 +26,23 @@ WOG.createIndex = (function () {
     };
 }());
 
+WOG.emptyForm = function () {
+    var i, inputs = WOG.formRow.getElementsByTagName("td");
+    for (i = 0; i < 5; i += 1) {
+        inputs[i].firstChild.value = "";
+    }
+    inputs[3].firstElementChild.checked = true;
+    inputs[5].firstChild.value = "add";
+    WOG.formRow.setAttribute("id", "formRow");
+};
+
 WOG.editFunction = function () {
     var row, rowIndex, cells = [], inputs = [], i;
-    row = WOG.doc.getElementById("editA");
+    row = WOG.doc.getElementById("editRow");
     if (row) {
         row.removeAttribute("id");
     }
+    this.disabled = true;
     row = this.parentNode.parentNode;
     rowIndex = row.rowIndex;
     cells = row.getElementsByTagName("td");
@@ -39,45 +52,65 @@ WOG.editFunction = function () {
     }
     cells[3].textContent === "lbs" ? (inputs[3].firstElementChild.checked = true) : (inputs[3].lastElementChild.checked = true);
     inputs[5].firstChild.value = "edit row";
-    row.setAttribute("id", "editA");
-    WOG.formRow.setAttribute("id", "editB");
+    row.setAttribute("id", "editRow");
+    WOG.formRow.setAttribute("id", "editForm");
 };
 
 WOG.deleteFunction = function () {
     var row = this.parentNode.parentNode.rowIndex;
-    console.log(this.id);
+    WOG.emptyForm();
     WOG.workoutTable.deleteRow(row);
 };
 
+WOG.fillRow = function (row, exName, reps, weight, units, date) {
+    var cells = row.getElementsByTagName("td");
+    cells[0].innerHTML = exName;
+    cells[1].innerHTML = reps;
+    cells[2].innerHTML = weight;
+    cells[3].innerHTML = units ? "lbs" : "kg";
+    cells[4].innerHTML = date;
+};
+
 WOG.createRow = function (exName, reps, weight, units, date) {
-    var row, i, cell = [], idNum = WOG.createIndex(), editButton, deleteButton;
+    var row, i, cell = [], idNum = WOG.createIndex(), editFormutton, deleteButton;
     row = WOG.workoutTBody.insertRow(0);
     for (i = 0; i < 6; i += 1) {
         cell[i] = row.insertCell(i);
     }
-    cell[0].innerHTML = exName;
-    cell[1].innerHTML = reps;
-    cell[2].innerHTML = weight;
-    cell[3].innerHTML = units ? "lbs" : "kg";
-    cell[4].innerHTML = date;
+    WOG.fillRow(row, exName, reps, weight, units, date);
     cell[5].innerHTML = '<input type="submit" value="edit" id="edit' + idNum + '"><input type="submit" value="delete" id="delete' + idNum + '">';
-    editButton = WOG.doc.getElementById("edit" + idNum);
-    editButton.addEventListener("click", WOG.editFunction);
+    editFormutton = WOG.doc.getElementById("edit" + idNum);
+    editFormutton.addEventListener("click", WOG.editFunction);
     deleteButton = WOG.doc.getElementById("delete" + idNum);
     deleteButton.addEventListener("click", WOG.deleteFunction);
 };
 
 WOG.bindSubmit = function () {
     WOG.submitButton.addEventListener("click", function (event) {
-        var request; 
+        var submitCurrentValue = WOG.submitButton.value; 
         event.preventDefault();
-        WOG.createRow(
-            WOG.doc.exercise.exerciseName.value,
-            WOG.doc.exercise.reps.value,
-            WOG.doc.exercise.weight.value,
-            WOG.doc.exercise.units.value === "lbs",
-            WOG.doc.exercise.date.value
-        );
+        if (submitCurrentValue === "add") {
+            WOG.createRow(
+                WOG.doc.exercise.exerciseName.value,
+                WOG.doc.exercise.reps.value,
+                WOG.doc.exercise.weight.value,
+                WOG.doc.exercise.units.value === "lbs",
+                WOG.doc.exercise.date.value
+            );
+        } else if (submitCurrentValue === "edit row") {
+            var editRow = WOG.doc.getElementById("editRow");
+            WOG.fillRow(
+                editRow,
+                WOG.doc.exercise.exerciseName.value,
+                WOG.doc.exercise.reps.value,
+                WOG.doc.exercise.weight.value,
+                WOG.doc.exercise.units.value === "lbs",
+                WOG.doc.exercise.date.value
+            );
+            editRow.removeAttribute("id");
+            editRow.lastElementChild.firstElementChild.disabled = false;
+        }
+        WOG.emptyForm();
     });
 };
 
